3137af93579d28d68affa1abccfd1afb47f354b2
Pavel Trush <paveltrush019@gmail.com>
2020-11-17 18:22:28 +0200

    test GetAccountDiscounts function

--- tests/Feature/App/Http/Controllers/Api/ClientController.php
+++ tests/Feature/App/Http/Controllers/Api/ClientController.php
@@ -70,5 +70,124 @@ class ClientController extends TestCase
             ]);
     }
 
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetAccountDiscountsByPhoneNotFound()
+    {
+        $data = ['user_phone' => '72942958613'];
+
+        $this->json('GET', '/api/GetAccountDiscountsPhone', $data)
+            ->assertStatus(404);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetAccountDiscountsByPhoneSuccessfullyIfNotEmptyResult()
+    {
+        $user_phone = Client::firstOrFail('personal_phone');
+
+        $data = ['user_phone' => $user_phone];
+
+        $this->json('GET', '/api/GetAccountDiscountsByPhone', $data)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    [
+                        'id',
+                        'first_name',
+                        'second_name',
+                        'last_name',
+                        'date_register',
+                        'last_login',
+                        'personal_mobile',
+                        'personal_phone',
+                        'discounts'
+                    ]
+                ]
+            ]);
+    }
+
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetAccountDiscountsByPhoneIfEmptyResult()
+    {
+        $data = ['user_phone' => '70000000000'];
+
+        $this->json('GET', '/api/GetAccountDiscountsByPhone', $data)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => []
+            ]);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetAccountDiscountNotFound()
+    {
+        $data = ['user_id' => null];
+
+        $this->json('GET', '/api/GetAccountDiscountByClient', $data)
+            ->assertStatus(404);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetAccountDiscountsIfNotEmptyResult()
+    {
+        $user_id = Client::first('id');
+
+        $data = ['user_id' => $user_id];
+
+        $this->json('GET', '/api/GetAccountDiscounts', $data)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    [
+                        'id',
+                        'first_name',
+                        'second_name',
+                        'last_name',
+                        'date_register',
+                        'last_login',
+                        'personal_mobile',
+                        'personal_phone',
+                        'discounts'
+                    ]
+                ]
+            ]);
+    }
+
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetAccountDiscountsIfEmptyResult()
+    {
+        $data = ['user_id' => null];
+
+        $this->json('GET', '/api/GetAccountDiscounts', $data)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => []
+            ]);
+    }
 
 }
