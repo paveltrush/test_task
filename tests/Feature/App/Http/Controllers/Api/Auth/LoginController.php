6a0e1de258a6e4d66112999bdd4bc732d90295bf
Pavel Trush <paveltrush019@gmail.com>
2020-11-17 17:01:22 +0200

    test login

--- tests/Feature/App/Http/Controllers/Api/Auth/LoginController.php
+++ tests/Feature/App/Http/Controllers/Api/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\App\Http\Controllers\Api\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +14,41 @@ class LoginController extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testRequiresEmailAndPassword()
     {
-        $response = $this->get('/');
+        $this->json('POST', 'api/login')
+            ->assertStatus(422)
+            ->assertJson([
+                'email' => ['The email field is required.'],
+                'password' => ['The password field is required.'],
+            ]);
+    }
 
-        $response->assertStatus(200);
+    public function testUserLoginsFail()
+    {
+        User::create([
+            'email' => 'borgan@gmail.com',
+            'password' => 'pass1234',
+        ]);
+
+        $user = ['email' => 'testlogin@user.com', 'password' => 'toptal123'];
+
+        $this->json('POST', 'api/login', $user)
+            ->assertStatus(401);
+    }
+
+    public function testUserLoginsSuccessfully()
+    {
+        User::create([
+            'email' => 'borgan@gmail.com',
+            'password' => 'pass1234',
+        ]);
+
+        $user = ['email' => 'borgan@gmail.com', 'password' => 'pass1234'];
+
+        $this->json('POST', 'api/login', $user)
+            ->assertStatus(200)
+            ->assertJsonCount(3, 'data');
+
     }
 }
