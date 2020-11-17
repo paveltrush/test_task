605dc2235d19c1bc220e22c86202aefa59eed687
Pavel Trush <paveltrush019@gmail.com>
2020-11-17 18:58:40 +0200

    test GetAllDiscounts function

--- tests/Feature/App/Http/Controllers/Api/DiscountController.php
+++ tests/Feature/App/Http/Controllers/Api/DiscountController.php
@@ -41,17 +41,15 @@ class DiscountController extends TestCase
             ->assertStatus(200)
             ->assertJsonStructure([
                 'data' => [
-                    [
-                        'id',
-                        'code',
-                        'name',
-                        'discount',
-                        'date_active_from',
-                        'date_active_to',
-                        'project_id',
-                        'preview_text',
-                        'users'
-                    ]
+                    'id',
+                    'code',
+                    'name',
+                    'discount',
+                    'date_active_from',
+                    'date_active_to',
+                    'project_id',
+                    'preview_text',
+                    'users'
                 ]
             ]);
     }
@@ -78,59 +76,64 @@ class DiscountController extends TestCase
      *
      * @return void
      */
-//    public function testGetAccountUsersNotFound()
-//    {
-//        $data = ['date_from' => '26.06.2019', 'date_to' => '27.06.2019'];
-//
-//        $this->json('GET', '/api/GetUsers', $data)
-//            ->assertStatus(404);
-//    }
-//
-//    /**
-//     * A basic feature test example.
-//     *
-//     * @return void
-//     */
-//    public function testGetAccountUsersSuccessfullyIfNotEmptyResult()
-//    {
-//        $dateFrom = Client::min('date_register');
-//        $dateTo = Client::max('date_register');
-//
-//        $data = ['date_from' => $dateFrom, 'date_to' => $dateTo];
-//
-//        $this->json('GET', '/api/GetAccountUsers', $data)
-//            ->assertStatus(200)
-//            ->assertJsonStructure([
-//                'data' => [
-//                    [
-//                        'id',
-//                        'first_name',
-//                        'second_name',
-//                        'last_name',
-//                        'date_register',
-//                        'last_login',
-//                        'personal_mobile',
-//                        'personal_phone',
-//                        'discounts'
-//                    ]
-//                ]
-//            ]);
-//    }
-//
-//
-//    /**
-//     * A basic feature test example.
-//     *
-//     * @return void
-//     */
-//    public function testGetAccountUsersSuccessfullyIfEmptyResult()
-//    {
-//        $data = ['date_from' => '26.06.2019', 'date_to' => '27.06.2019'];
-//
-//        $this->json('GET', '/api/GetAccountUsers', $data)
-//            ->assertStatus(200)
-//            ->assertJsonStructure([
-//                'data' => []
-//            ]);
-//    }
+    public function testGetAllDiscountsNotFound()
+    {
+        $this->json('GET', '/api/GetUsers', null)
+            ->assertStatus(404);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetAllDiscountsWithEmptyParameterSuccessfully()
+    {
+        $this->json('GET', '/api/GetAllDiscounts', null)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    [
+                        'id',
+                        'code',
+                        'name',
+                        'discount',
+                        'date_active_from',
+                        'date_active_to',
+                        'project_id',
+                        'preview_text',
+                        'users'
+                    ]
+                ]
+            ]);
+    }
+
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetAllDiscountsWithNotEmptyParameterSuccessfully()
+    {
+        $discountId = Discount::first('id');
+
+        $data = ['discount_id' => $discountId];
+
+        $this->json('GET', '/api/GetAllDiscounts', $data)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'code',
+                    'name',
+                    'discount',
+                    'date_active_from',
+                    'date_active_to',
+                    'project_id',
+                    'preview_text',
+                    'users'
+                ]
+            ]);
+    }
 }
